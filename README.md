bb2854d9b94869d6b6171014c1262931132e4781
Mercedes <mermfb@gmail.com>
2021-03-03 18:35:08 +0100

    Add initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Evaluación intermedia modulo 3 Adalab
+
+En este repositorio encontrarás mi solución al ejercicio de evaluación intermedia del módulo 3 de Adalab.
+Este ejercicio ha sido resuelto con React, utilizando componentes de clase y componentes funcionales.
---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import "../stylesheets/App.scss";
+import { Link, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Rick and Morty</h1>
+    </div>
+  );
+}
+
+export default App;
